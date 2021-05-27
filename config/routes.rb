bc067fcb8c97e0f0ea8b237318f7acfc3219e89f
Mgenki1104 <chobichobin0006@gmail.com>
2021-05-27 13:50:38 +0000

    (finish)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/show'
-  get 'home/new'
-  get 'home/edit'
+  get 'homes/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root :to => 'books#top'
+root :to => 'homes#top'
 resources :books
 # get 'books/new'
 #get 'top' => 'books#top'
